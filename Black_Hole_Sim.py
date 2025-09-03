56d0b26f7beeaab1240cfaeb13598d1c3a4b5a3e
Maaz Khokhar <khokharmaaz@gmail.com>
2025-09-02 21:56:22 -0500

    Implement main simulation loop and add Blackhole class skeleton

--- Black_Hole_Sim.py
+++ Black_Hole_Sim.py
@@ -1,2 +1,20 @@
 import pygame
 import pygame_widgets
+
+pygame.init()
+
+WIN = pygame.display.set_mode((800, 600))
+pygame.display.set_caption("Black Hole Simulator")
+
+clock = pygame.time.Clock()
+on = True
+FPS = 165
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            on = False
+            break
+
+    print("askdasd")
+    clock.tick(FPS)
