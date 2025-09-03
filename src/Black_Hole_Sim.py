ac125f1ba76283b062936bf1490f3ba03e5c0cbb
Maaz Khokhar <khokharmaaz@gmail.com>
2025-09-02 22:11:01 -0500

    Refactor main function to encapsulate initialization and event loop

--- src/Black_Hole_Sim.py
+++ src/Black_Hole_Sim.py
@@ -1,22 +1,26 @@
 import pygame
 import pygame_widgets
 
-pygame.init()
+def main() -> None:
+    pygame.init()
 
-WIN = pygame.display.set_mode((800, 600))
-ICON = pygame.image.load("icon.png")
+    WIN = pygame.display.set_mode((800, 600))
+    ICON = pygame.image.load("icon.png")
+    
+    pygame.display.set_caption("Black Hole Simulator")
+    pygame.display.set_icon(ICON)
+    
+    clock = pygame.time.Clock()
+    on = True
+    FPS = 165
+    
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                on = False
+                break
+            
+        clock.tick(FPS)
 
-pygame.display.set_caption("Black Hole Simulator")
-pygame.display.set_icon(ICON)
-
-clock = pygame.time.Clock()
-on = True
-FPS = 165
-
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            on = False
-            break
-
-    clock.tick(FPS)
+if __name__ == "__main__":
+    main()
